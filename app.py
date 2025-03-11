2d6a61064263b6a4fe4675f70bde057201e136aa
rohanxfin <rohan.khandelwal@nxfin.in>
2025-03-11 18:59:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -213,4 +213,6 @@ def number_plate_removal():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+        import os
+        port = int(os.getenv("PORT", 8080))
+        app.run(host="0.0.0.0", port=port)
